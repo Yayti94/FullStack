f6e0ab44520f4059a2fd002c79a73fceeeaf6487
Yatish Sahu <68045812+Yayti94@users.noreply.github.com>
2020-07-09 14:11:50 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,20 @@
+document.getElementById("m1").addEventListener("click", function(){
+    document.getElementById("m1").style.display="none";
+      document.getElementById("c5").style.display="block";
+    });
+
+document.getElementById("i1").addEventListener("click", function(){
+    document.getElementById("c5").style.display="none";
+    document.getElementById("m1").style.display="block";
+    });    
+
+document.getElementById("div1").addEventListener("click", function(){
+    document.getElementById("div1").style.display="none";
+      document.getElementById("div2").style.display="block";
+    });
+
+
+document.getElementById("div3").addEventListener("click", function(){
+    document.getElementById("div3").style.display="none";
+      document.getElementById("div4").style.display="block";
+    });
